352b9d6d3fa936a7f082cb5653d90ccce942da82
JPolanco <jdanypa@gmail.com>
2018-12-06 23:27:24 -0600

    Fixed File model when is getting the data of the file

--- Modules/Models/FileModel.py
+++ Modules/Models/FileModel.py
@@ -1,16 +1,25 @@
-import os
+from os import path
 
 
 class File:
     def __init__(self, _file_name: str, _file_path: str):
         self.file_name = _file_name
         self.file_path = _file_path
-        self.file_available = False
-        try:
-            controller = os
-            controller.chdir(self.file_path)
-            self.file_data = open(_file_name, "r").read()
-            self.file_available = True
+        self.file_data = ""
+        filePath = _file_path + "\\" + _file_name
+        self.file_available = path.exists(filePath)
+        if self.file_available:
+            try:
+                access = open(filePath, "r")
+                self.file_data = access.read()
+                access.close()
+            except FileNotFoundError:
+                self.file_data = ""
 
-        except FileNotFoundError:
-            self.file_data = ""
+    def __str__(self) -> str:
+        data = "filename: " + self.file_name + "\n\r" + \
+               "path    : " + self.file_path + "\n\r" + \
+               "exist   : " + self.file_available.__str__() + "\n\r"
+        if self.file_data != "":
+            data += "data    : " + self.file_data + "\n\r"
+        return data
--- QuantumRQ.py
+++ QuantumRQ.py
@@ -1,7 +1,12 @@
+import os
+
 from Modules.ConsoleLoger import ConsoleLoger
 from Modules.FileManager import FileManager
+from Modules.Models.FileModel import File
 
 # Testing modules...
 ConsoleLoger.Info("Testing")
 FileManager.create("text.txt")
 ConsoleLoger.Ok("Archivo creado")
+test = File("text.txt", os.getcwd())
+print(test.__str__())
